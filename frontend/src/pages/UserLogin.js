04aa5152e6dd31d7f982a38491235fffd9f8c05f
Aapka Naam <aapkaemail@example.com>
2025-10-30 21:09:39 +0530

    update

--- frontend/src/pages/UserLogin.js
+++ frontend/src/pages/UserLogin.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axiosInstance from "../api/axiosInstance"; 
-import { registerUserSocket } from "./usersocket"; // ✅ import user socket
+import { registerUserSocket } from '../userSocket'; // note capital S
 import styles from "./UserLogin.module.css";
 
 const UserLogin = () => {
